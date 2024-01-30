499b9cb0bf0b85917b10c45da83780ba8595ac9e
Bartek <bartekwieteckiyt@gmail.com>
2024-01-30 22:17:53 +0100

    updated demo react web app

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,8 +1,10 @@
-function TabButton({children, onSelect}) {
+function TabButton({ children, onSelect, isSelected }) {
 
     return (
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
+                {children}
+            </button>
         </li>
     );
 }
